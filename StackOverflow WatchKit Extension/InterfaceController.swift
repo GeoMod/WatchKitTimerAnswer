df62ca08a09097bdb4c945f7697d27fbb21e7b5e
GeoMod <olearymn@gmail.com>
2017-08-31 13:58:08 -0700

    Initial Commit. Meant to aid in answering question regarding WatchKit timers on Stack Overflow.

--- StackOverflow WatchKit Extension/InterfaceController.swift
+++ StackOverflow WatchKit Extension/InterfaceController.swift
@@ -9,19 +9,49 @@
 import WatchKit
 import Foundation
 
-
 class InterfaceController: WKInterfaceController {
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
-        // Configure interface objects here.
     }
+    
+    var theTimer = Timer()
+    var backgroundTimer = TimeInterval(15)
+    @IBOutlet var appleTimer: WKInterfaceTimer!
+    @IBOutlet var label: WKInterfaceLabel!
+    
     
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        appleTimer.setDate(Date(timeIntervalSinceNow: 15))
+        label.setText("")
+    }    
+    
+    
+    @IBAction func startButton() {
+        let startTime = Date(timeIntervalSinceNow: 15)
+        appleTimer.setDate(startTime)
+        appleTimer.start()
+        
+        // This will call timerCountDown() once per second until conditions are met.
+        theTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timerCountDown), userInfo: nil, repeats: true)
+        
     }
+    
+    func timerCountDown() {
+        backgroundTimer -= 1.0
+        print(backgroundTimer)
+        if backgroundTimer == 0 {
+            theTimer.invalidate()
+            appleTimer.stop()
+            
+            // You could call an Alert Action here.
+            label.setText("Timer Done!")
+        }
+        
+    }
+
     
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
